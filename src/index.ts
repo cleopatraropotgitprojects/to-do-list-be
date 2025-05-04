812b3e94ae858fbbe5ae26f9ebe0f227550a7e44
cleopatraropot <cleopatraropot@gmail.com>
2025-05-04 13:00:16 +0300

    Fix CORS config: add Authorization to allowedHeaders

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,12 @@ const app = express();
 app.use(
     cors({
         origin: "http://localhost:3000",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type"],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
     })
 );
+
 app.use(express.json());
 
 app.use("/tasks", taskRoutes);
